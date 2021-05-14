aa41c0de1bbded48fbef2c9bd8bef92d1ef15314
traviscoan <t.coan@exeter.ac.uk>
2021-05-14 13:45:44 +0100

    Add utils

--- fit/logistic.py
+++ fit/logistic.py
@@ -1,47 +1,46 @@
 """Fit the optimised logistic classifer to the CARDS corpus."""
 
-import pandas as pd
-import pickle
 from sklearn import preprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
-# Read training data
-df_train = pd.read_json('data/training_data/train.json')
 
-# DOOKIE: reomove need for this preprocessing
-df_train['sub_claim_combined'] = df_train['sub_claim_combined'].astype('str')
-df_train['sub_claim_combined'] = df_train['sub_claim_combined'].str[:3]
-df_train['sub_claim_combined'] = df_train['sub_claim_combined'].str.replace('\.', '_')
-cards_train = df_train.to_dict(orient='records')
+def fit_logistic_classifier(data):
+    '''
+    Takes training "data" and fits a LogisticRegression classifier.
 
-# Encode labels
-claims = [str(row['sub_claim_combined']) for row in cards_train]
-le = preprocessing.LabelEncoder()
-y = le.fit_transform(claims)
+    Args:
+        data (list): A list of lists holding the paragraph text (index 0) 
+                     and the claim (index 1).
+    
+    Returns:
+        dict: A dictionary with the following keys:
+              "clf" (LogisticRegression): the fitted logistic classifer
+              "label_encoder" (LabelEncoder): encoder used for labels.
+              "vectorizer" (TfidfVectorizer): Fitted TF-IDF vectorizer.           
+    '''
+    # Encode labels
+    claims = [row[1] for row in data]
+    le = preprocessing.LabelEncoder()
+    y = le.fit_transform(claims)
 
-# Vectorize
-text = [row['tokens'] for row in cards_train]
-vectorizer = TfidfVectorizer(min_df=3,  max_features=None,
-                             strip_accents='unicode',
-                             ngram_range=(1, 2), use_idf=1, smooth_idf=1, sublinear_tf=1)
+    # Vectorize
+    text = [row[0] for row in data]
+    vectorizer = TfidfVectorizer(min_df=3,  max_features=None,
+                                strip_accents='unicode',
+                                ngram_range=(1, 2), use_idf=1, smooth_idf=1, sublinear_tf=1)
 
-X = vectorizer.fit_transform(text)
+    X = vectorizer.fit_transform(text)
 
+    # Fit final logistic classifier. Hyperparameters tuned via grid search using
+    #  10-fold cross-validation
+    clf_logit = LogisticRegression(C=7.96,
+                                solver='lbfgs',
+                                multi_class='ovr',
+                                max_iter=200,
+                                class_weight='balanced')
 
-# Fit final logistic classifier. Hyperparameters tuned via grid search using
-#  10-fold cross-validation
-clf_logit = LogisticRegression(C=7.96,
-                               solver='lbfgs',
-                               multi_class='ovr',
-                               max_iter=200,
-                               class_weight='balanced')
+    # Fit final logit model
+    clf_logit.fit(X, y)
 
-# Fit final logit model
-clf_logit.fit(X, y)
-
-# Save model
-model = {'clf': clf_logit, 'label_encoder': le, 'vectorizer': vectorizer}
-
-with open('classifiers/fitted_models/logistic.pkl', 'wb') as f:
-    pickle.dump(model, f)
+    return {'clf': clf_logit, 'label_encoder': le, 'vectorizer': vectorizer}
